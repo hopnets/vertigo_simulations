7678853d7043f4e90bf6f9ad9d55f8e44ddf13ea
Sepehr Abdous <sabdous1@jh.edu>
2022-02-11 14:55:36 -0500

    change download_dist_files

--- Omnet_Sims/dc_simulations/simulations/Vertigo_Sims/download_dist_files.sh
+++ Omnet_Sims/dc_simulations/simulations/Vertigo_Sims/download_dist_files.sh
@@ -2,13 +2,21 @@ echo "Downloading dist files..."
 
 mkdir distributions
 
-bash dist_downloader_files/dist_downloader_large_fattree_cache_bg_web_incast.sh < dist_downloader_files/dist_file_names_large_fattree_cache_bg_web_incast.csv
-
-bash dist_downloader_files/dist_downloader_large_leaf_spine_web_incast.sh < dist_downloader_files/dist_file_names_large_leaf_spine_web_incast.csv
-
-bash dist_downloader_files/dist_downloader_parallel_large_cache_bg_web_incast_extra_files.sh < dist_downloader_files/parallel_large_cache_bg_web_incast_extra_files.csv
+git clone https://sepehrabdous96@bitbucket.org/dc_group/vertigo_dist_files_conext21.git distributions
 
 echo "Extracting dist files!"
+
+unzip distributions/files.zip 
+unzip distributions/large_fat_tree.zip
+unzip distributions/parallel_large_cache_bg_web_incast_extra_files.zip
+
+mv files/* distributions/
+mv large_fat_tree/* distributions/
+mv parallel_large_cache_bg_web_incast_extra_files/* distributions/
+
+rm -rf files
+rm -rf large_fat_tree
+rm -rf parallel_large_cache_bg_web_incast_extra_files
 
 bunzip2 -vf ./distributions/*.bz2
 
