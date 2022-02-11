d5e5eae28b4cff17ee771e628bba9d505f666645
Sepehr Abdous <sabdous1@jh.edu>
2022-02-11 16:53:27 -0500

    minor update

--- Omnet_Sims/dc_simulations/simulations/Vertigo_Sims/download_dist_files.sh
+++ Omnet_Sims/dc_simulations/simulations/Vertigo_Sims/download_dist_files.sh
@@ -9,14 +9,17 @@ echo "Extracting dist files!"
 unzip distributions/files.zip 
 unzip distributions/large_fat_tree.zip
 unzip distributions/parallel_large_cache_bg_web_incast_extra_files.zip
+unzip distributions/small_dqps_cache_bg_web_incast.zip
 
 mv files/* distributions/
 mv large_fat_tree/* distributions/
 mv parallel_large_cache_bg_web_incast_extra_files/* distributions/
+mv small_dqps_cache_bg_web_incast/* distributions/
 
 rm -rf files
 rm -rf large_fat_tree
 rm -rf parallel_large_cache_bg_web_incast_extra_files
+rm -rf small_dqps_cache_bg_web_incast
 
 bunzip2 -vf ./distributions/*.bz2
 
